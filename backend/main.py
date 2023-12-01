07233405bbc52aba31eb5d8387f1c47db3cd7eb1
Florian Remberger <flo.remberger@gmail.com>
2023-12-01 22:00:12 +0100

    Initial commit

---
+++ backend/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+# Mounting the "frontend" directory to serve static files(HTML, CSS, JS)
+app.mount("/static", StaticFiles(directory="../frontend"), name="/static")
+
+# Template configuration for Jinja2
+# templates = Jinja2Templates(directory="../frontend")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root():
+    return HTMLResponse(content="/static/index.html", status_code=200)
+
+
+
+
