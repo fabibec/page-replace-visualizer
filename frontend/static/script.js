3dff08ab07bee1d667c101ce1f1e041be5b88ec4
Fabian Becker <fabian.becker@st.oth-regensburg.de>
2023-12-13 15:45:13 +0100

    Chart js example

--- frontend/static/script.js
+++ frontend/static/script.js
@@ -47,3 +47,25 @@ document.getElementById('faultsCompareBtn').addEventListener('click', function (
                     })
         .catch(err => console.log(err));
 });
+
+// Chart js test
+const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+});
