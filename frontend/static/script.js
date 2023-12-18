b7e7f8cf1bcc8b97128c01788ef58f2c833c726f
Fabian Becker <fabian.becker@st.oth-regensburg.de>
2023-12-18 22:58:33 +0100

    Fixed some CSS Bugs, hardened validation, styled imprint

--- frontend/static/script.js
+++ frontend/static/script.js
@@ -1,36 +1,17 @@
-function flagRefStrInptField(){
-  let el = refStrInpt;
-  let msg = document.getElementById('refStrInptMsg');
-  if (el.value) {
-    el.parentElement.classList.add('has-success');
-    el.parentElement.classList.remove('has-error');
-    msg.style.display = 'none';
-  } else {
-    el.parentElement.classList.remove('has-success');
-    el.parentElement.classList.add('has-error');
-    msg.style.display = 'block';
-  }
-}
-
-// Updating RefString Slider
 function updateSliderValue(sliderID, valueID){
   let length = document.getElementById(sliderID).valueAsNumber;
   document.getElementById(valueID).innerHTML = '<b>' + length + '</b>';
 }
 
-function noEnter() {
-  return !(window.event && window.event.keyCode == 13);
+function toggleAllCheckboxes(source, destName) {
+  for (i = 0; i < document.getElementsByName(destName).length; i++)
+      document.getElementsByName(destName)[i].checked = source.checked;
 }
 
 function init(){
   updateSliderValue('refStrSize', 'refStrSizeValue');
   updateSliderValue('faultsFrameSize', 'FrameSizeValue');
   updateSliderValue('memoryFrameSize', 'memoryFrameSizeValue');
-}
-
-function toggleAllCheckboxes(source, destName) {
-    for (i = 0; i < document.getElementsByName(destName).length; i++)
-        document.getElementsByName(destName)[i].checked = source.checked;
 }
 
 init();
