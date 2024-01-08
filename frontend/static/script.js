5780ddfdc4f7573d0785d6eb8f97be926dc0c8db
Florian Remberger <flo.remberger@gmail.com>
2024-01-08 21:43:55 +0100

    Solve problem.

--- frontend/static/script.js
+++ frontend/static/script.js
@@ -8,6 +8,22 @@ function toggleAllCheckboxes(source, destName) {
       document.getElementsByName(destName)[i].checked = source.checked;
 }
 
+function onFaultCheckboxClick(destName) {
+    onCheckboxClick('checkAllFaults', destName);
+}
+function onFaultRangeCheckboxClick(destName) {
+    onCheckboxClick('checkAllFaultsRange', destName);
+}
+function onCheckboxClick(checkboxId, destName) {
+    for (i = 0; i < document.getElementsByName(destName).length; i++) {
+        if(!document.getElementsByName(destName)[i].checked){
+          document.getElementById(checkboxId).checked = false;
+          return;
+        }
+    }
+    document.getElementById(checkboxId).checked = true;
+}
+
 function init(){
   updateSliderValue('refStrSize', 'refStrSizeValue');
   updateSliderValue('faultsFrameSize', 'FrameSizeValue');
